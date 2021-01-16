abc4b25ab5cbfeb4acea8684315bca6cd8ff7264
Eagle owner <73874131+noor-ali-346@users.noreply.github.com>
2021-01-16 17:06:42 +0500

    Update global_bans.py

--- SaitamaRobot/modules/global_bans.py
+++ SaitamaRobot/modules/global_bans.py
@@ -139,7 +139,7 @@ def gban(update: Update, context: CallbackContext):
 
         return
 
-    message.reply_text("HAHA GBAN TIME!")
+    message.reply_text("#GBAN!")
 
     start_time = time.time()
     datetime_fmt = "%Y-%m-%dT%H:%M"
